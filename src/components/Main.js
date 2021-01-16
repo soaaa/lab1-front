df332674cdabbca0b7976eeb6bd559efd7ccc487
cdu <you@example.com>
2021-01-16 08:25:18 +0000

    Add HeadingsRow component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,19 @@
+import HeadingsRow from "./HeadingsRow";
+
+function onOrdersChanged(orders) {
+  console.log(JSON.stringify(orders));
+}
+
 function Main() {
-  return <div className="Main"></div>;
+  return (
+    <div className="Main">
+      <table>
+      <thead>
+        <HeadingsRow onChanged={onOrdersChanged} />
+      </thead>
+      </table>
+    </div>
+  );
 }
 
 export default Main;
