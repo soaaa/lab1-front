e67ac057cd70aee8daed46aedcec72c745ed1407
cdu <you@example.com>
2021-01-21 19:58:46 +0000

    Implement AvgFuelConsumption comp

--- src/components/AvgFuelConsumption.js
+++ src/components/AvgFuelConsumption.js
@@ -1,5 +1,49 @@
-function AvgFuelConsumption() {
-  return <div className="AvgFuelConsumption"></div>;
+import "./css/AvgFuelConsumption.css";
+
+import React from "react";
+
+import Api from "../api";
+
+class AvgFuelConsumption extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { value: null };
+  }
+
+  componentDidMount() {
+    Api
+      .findAvgFuelConsumption()
+      .then(res => {
+        if (res.status === 200) {
+          this.setState({ value: res.data });
+        } else {
+          alert(res.data);
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
+  getValueOrEmpty() {
+    if (this.state.value === null) return;
+    return this.state.value;
+  }
+
+  render() {
+    return (
+      <div className="AvgFuelConsumption">
+        <table>
+        <tbody>
+          <tr>
+            <td>Average fuel consumption</td>
+            <td>{this.getValueOrEmpty()}</td>
+          </tr>
+        </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default AvgFuelConsumption;
