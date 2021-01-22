298df2a920f1b59536155f309a9973e4260103f2
cdu <you@example.com>
2021-01-22 11:05:29 +0000

    Import bootstrap styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './components/App';
 
 ReactDOM.render(
